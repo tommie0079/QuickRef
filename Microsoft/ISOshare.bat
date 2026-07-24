483a303e94fa1dfbe9c769db2e581a59f199628c
tommie0079 <t.lentjes@hotmail.com>
2026-07-24 08:38:33 +0200

    Update ISOshare.bat

--- Microsoft/ISOshare.bat
+++ Microsoft/ISOshare.bat
@@ -1,2 +1,6 @@
-net use \\192.168.1.2\ISOs /user:192.168.1.2\iso LAB_admin123
-explorer \\192.168.1.2\ISOs
+@echo off
+REM Map the ISO share to drive Z: and keep it across reboots.
+REM Remove any existing Z: mapping first so re-running doesn't error.
+net use Z: /delete /yes >nul 2>&1
+net use Z: \\192.168.1.2\ISOs /user:192.168.1.2\iso LAB_admin123 /persistent:yes
+explorer Z:
